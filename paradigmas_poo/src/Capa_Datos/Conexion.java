c6697e24197fae864dd17cbd8ac31b72a4ecd662
JAGR1792 <jorgenopehd@gmail.com>
2025-02-22 13:15:57 -0500

    Add files via upload

---
+++ paradigmas_poo/src/Capa_Datos/Conexion.java
@@ -0,0 +1,42 @@
+package Capa_Datos;
+import java.sql.*;
+
+public class Conexion {
+    private final String url = "jdbc:mysql://localhost:3306/ventas";
+    private final String usuario = "root";
+    private final String pwd = "";
+
+    public Conexion() {}
+
+    public ResultSet Listar(String Cad){
+        try{
+            Connection cn = DriverManager.getConnection(url, usuario, pwd);
+            PreparedStatement da = cn.prepareStatement(Cad);
+            ResultSet tbl = da.executeQuery();
+            return tbl;
+        }
+        catch (SQLException e) {
+            javax.swing.JOptionPane.showMessageDialog(null, e.getMessage());
+            return null;
+        }
+    }
+
+    public String Ejecutar(String Cad){
+        try{
+            Connection cn = DriverManager.getConnection(url, usuario, pwd);
+            PreparedStatement da = cn.prepareStatement(Cad);
+            int r = da.executeUpdate();
+            if (r > 1){
+                return "Se afecto " + r + " fila";
+            }
+            else {
+                return "No se afecto ninguna fila";
+            }
+
+        }
+        catch (SQLException e) {
+            javax.swing.JOptionPane.showMessageDialog(null, e.getMessage());
+            return "Error " + e.getMessage();
+        }
+    }
+}
